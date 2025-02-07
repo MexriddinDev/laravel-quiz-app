227c73a6179fb5645d3dee9cfe4fe6ff2ef4a71c
Mexriddin <mekkinuriddinov@gmail.com>
2025-02-07 14:26:06 +0500

    views added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/take-quiz', function () {
+    return view('take-quiz');
+});
